d4c33b3ea348249af5ec7c910d0711ed70e23ab2
Christopher Smith <csmith@keepersecurity.com>
2025-08-04 10:31:36 -0500

    test: cover ksm auto config and property locator

--- integration/spring-boot-starter-keeper-ksm/src/test/java/com/keepersecurity/spring/ksm/autoconfig/KeeperKsmAutoConfigurationTest.java
+++ integration/spring-boot-starter-keeper-ksm/src/test/java/com/keepersecurity/spring/ksm/autoconfig/KeeperKsmAutoConfigurationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(classes = KeeperKsmAutoConfiguration.class,
@@ -17,5 +18,7 @@ class KeeperKsmAutoConfigurationTest {
     @Test
     void contextLoads() {
         assertNotNull(options, "SecretsManagerOptions bean should be initialized");
+        String clientId = options.getStorage().getString("clientId");
+        assertEquals("client-id", clientId, "clientId should be loaded from config file");
     }
 }
